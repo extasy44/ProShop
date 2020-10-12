a418d54f8faa979a4a3d904aa29c3a2b20aee46b
extasy44 <seoheejun@gmail.com>
2020-10-12 15:26:06 +1100

    set up redux, fetching product list, details from redux store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
+import { listProducts } from '../actions/productActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
 
-    fetchProducts();
-  }, []);
+    useEffect(() => {
+        listProducts(dispatch);
+    }, [dispatch]);
 
-  return (
-    <>
-      <h1> Lastest Products</h1>
-      <Row>
-        {products &&
-          products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-      </Row>
-    </>
-  );
+    return (
+        <>
+            <h1> Lastest Products</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Row>
+                    {products &&
+                        products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                </Row>
+            )}
+        </>
+    );
 };
 
 export default HomeScreen;
